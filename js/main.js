8bf27194055d62176d7559049eceb7400746f953
Yousif-A2 <yf.alnasser@gmail.com>
2023-06-23 20:00:29 +0300

    Fix skills js

--- js/main.js
+++ js/main.js
@@ -29,23 +29,25 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 // Accordion Skills
-const skillsContent = document.getElementsByClassName('skills__content'),
-skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
 
-    for(i=0; i<skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.ClassName = 'skills__content skills__open'
-    }
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
-})
+  el.addEventListener('click', toggleSkills);
+});
+
 
 // Qualification Tabs
 const tabs = document.querySelectorAll('[data-target]'),
